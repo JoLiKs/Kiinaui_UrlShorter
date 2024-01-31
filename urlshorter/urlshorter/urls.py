642cdbd69420eb7287774a40e78ef9f459b3f5d7
Egor Ralovich <vonkad002@gmail.com>
2024-01-31 23:45:20 +0300

    Update urls.py

--- urlshorter/urlshorter/urls.py
+++ urlshorter/urlshorter/urls.py
@@ -21,5 +21,6 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('adminos/', views.adminos),
     path('<str:short_url>', views.redirection)
 ]
